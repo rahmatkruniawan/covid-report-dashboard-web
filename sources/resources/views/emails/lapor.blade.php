9449cf5f4aad630c624f58188cc1cfc2aa9638de
yasseryazid <yasser@jakpat.net>
2021-04-30 10:18:43 +0700

    main: email template

---
+++ sources/resources/views/emails/lapor.blade.php
@@ -0,0 +1,10 @@
+<h3>Yth {{ $name }}</h3>
+
+<p>Terimakasih atas laporan yang sudah anda sampaikan, kami akan menindaklanjuti proses selanjutnya
+untuk menangani kasus atau masalah tersebut. Berikut kode akses yang dapat anda gunakan 
+dalam proses pencarian laporan yang sudah disampaikan : <br> 
+"KODE LAPORAN {{ $content }}"</p>
+
+<p>Selalu jaga kesehatan dan tetap menerapkan protokol kesehatan dimanapun anda berada !</p>
+
+<p>Hormat Kami,<br>Satgas Covid-19 Yogyakarta</p>
---
+++ sources/resources/views/emails/progress.blade.php
@@ -0,0 +1,2 @@
+<h3>Yth. {{ $name }}</h3>
+<p>Progres laporan kamu sampai tahap {{ $content }}. Terima Kasih</p>
